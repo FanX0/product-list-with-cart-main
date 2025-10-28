e5772dce3be6cf09e7e4c81cbcf5f37986eee6b5
fanx <faridazhari46@gmail.com>
2025-10-28 17:22:56 +0700

    fix card content tittle

--- src/components/card/CardContent.tsx
+++ src/components/card/CardContent.tsx
@@ -4,9 +4,11 @@ const CardContent = ({ item }: { item: Data }) => {
   console.log(item.name);
   return (
     <div className="relative">
-      <p>{item.category}</p>
-      <p>{item.name}</p>
-      <p>{item.price}</p>
+      <p className="text-Rose-500 font-normal text-[0.8rem]">{item.category}</p>
+      <p className="text-[1rem] font-semibold">{item.name}</p>
+      <p className="text-[1rem] text-Rose-500 font-semibold">
+        ${item.price.toFixed(2)}
+      </p>
     </div>
   );
 };
