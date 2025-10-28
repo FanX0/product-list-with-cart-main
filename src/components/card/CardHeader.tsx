d10a8587f42be037e33cc8a6d4e0f3b3aa0b8e08
fanx <faridazhari46@gmail.com>
2025-10-28 17:34:07 +0700

    add responsive image

--- src/components/card/CardHeader.tsx
+++ src/components/card/CardHeader.tsx
@@ -11,11 +11,29 @@ const CardHeader = ({
 }: CardProps) => {
   return (
     <div className="relative flex flex-col items-center">
-      <img
-        className={selected && "border-2 border-Red rounded-lg"}
-        src={item.image.mobile}
-        alt={item.name ?? ""}
-      />
+      <picture>
+        <source
+          srcSet={item.image.mobile}
+          type="image/jpeg"
+          media="(max-width: 37.5rem)"
+        />
+        <source
+          srcSet={item.image.tablet}
+          type="image/jpeg"
+          media="(max-width: 102.4rem)"
+        />
+        <source
+          srcSet={item.image.desktop}
+          type="image/jpeg"
+          media="(min-width: 102.5rem)"
+        />
+
+        <img
+          src={item.image.desktop}
+          alt={item.name ?? ""}
+          className={selected ? "border-2 border-Red rounded-lg" : ""}
+        />
+      </picture>
 
       <div className="absolute bottom-[-1.5rem] flex items-center justify-center rounded-full gap-[0.5rem] w-[10rem] h-[3rem] border-1 border-gray-400 overflow-hidden">
         {selected ? (
